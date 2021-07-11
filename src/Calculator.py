9bed17377133f6ef51c814aa0cd2be9b32b8a3fa
vinayb004 <67874722+vinayb004@users.noreply.github.com>
2021-07-11 19:48:09 -0400

    Task :created Calculator with add and subtract

---
+++ src/Calculator.py
@@ -0,0 +1,18 @@
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+
+
+class Calculator:
+
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
