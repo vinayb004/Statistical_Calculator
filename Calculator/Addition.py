fdb6f305faa7b19ec435253d016d4c7a9d11af3e
vinayb004 <67874722+vinayb004@users.noreply.github.com>
2021-07-11 19:53:55 -0400

    Task :Added the addition and subtraction files

---
+++ Calculator/Addition.py
@@ -0,0 +1,7 @@
+def addition(a, b):
+    if (isinstance(a, float) and isinstance(b, float)) or (isinstance(a, float) and isinstance(b, int)) or (isinstance(a, int) and isinstance(b, float)):
+        return float(a) + float(b)
+    elif isinstance(a, int) and isinstance(b, int):
+        return int(a) + int(b)
+    else:
+        raise Exception("Data type not supported for addition operation!")
