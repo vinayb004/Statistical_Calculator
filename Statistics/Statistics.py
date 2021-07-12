1d6c2e607c5a8e8881e21f9aab94b797ea4a0d7a
vinayb004 <67874722+vinayb004@users.noreply.github.com>
2021-07-12 11:23:56 -0400

    Task :Updated Statistics with Median and Mode

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,5 +1,7 @@
 from Calculator.Calculator import Calculator
 from Statistics.Mean import get_mean
+from Statistics.Median import get_median
+from Statistics.Mode import get_mode
 
 class Statistics(Calculator):
 
@@ -8,4 +10,13 @@ class Statistics(Calculator):
 
     def stats_mean(self, data):
         self.result = get_mean(data)
+        return self.result
+
+    def stats_median(self, data):
+        data.sort()
+        self.result = get_median(data)
+        return self.result
+
+    def stats_mode(self, data):
+        self.result = get_mode(data)
         return self.result
